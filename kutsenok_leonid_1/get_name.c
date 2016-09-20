7872fac3caf753d55721c6178921fd67ee9cc32a
Leonid Kutsenok <leo@sochi.com>
2016-09-20 21:13:09 +0300

    Код рабочий, все функции в одном файле (пока)

--- kutsenok_leonid_1/get_name.c
+++ kutsenok_leonid_1/get_name.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-char
+#define MAX_INPUT 100;
+
+char * get_name();
+void print_str(char*);
 
 int main() {
-	int c;
-	int i = 0;
-	char input[100];
-	while((c = getchar()) != EOF) {
-		input[i] = c;
-		i++;
-	}
-	input[i] = '\0';
-	printf("%s", input);
+  char * input = get_name();
+  char output[107] = "Hello, ";
+  strcat(output, input);
+  print_str(output);
+}
+
+char * get_name() { // * - указатель на массив
+  int c;
+  int i = 0;
+  //char * input = malloc(100); //работает так
+  static char input[100]; /*и так тоже (static
+    оставляет место в пямяти)*/
+  while((c = getchar()) != EOF) {
+          input[i] = c;
+          i++;
+  }
+  input[i] = '\0'; //надо же как-то вывод завершить
+  return input;
+}
+
+void print_str(char * output) {
+  printf("%s\n", output);
 }
