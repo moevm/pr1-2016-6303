cb3e32e5e392197d50a260637ab7ec6c0e1b2e3a
Gorbunova <alinagorbunova98@mail.ru>
2016-10-06 20:49:40 +0300

    Update main.c

--- sergeenkov_michael/main.c
+++ sergeenkov_michael/main.c
@@ -3,10 +3,10 @@
 #include <string.h>
 #include "get_name.h"
 #include "print_str.h"
-int main (void){
+int main (void) {
 	char hello[90]="Hello, ";
 	char* result;
 	result=get_name();
 	print_str(strncat(hello, result, 80));
-	return 0;	
+	return ;	
 }
