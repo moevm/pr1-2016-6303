08a976d69107bbd1dafa4918c0b8a3084c2e902e
Grechkov_s <stas.grechkov@mail.ru>
2016-10-07 18:44:05 +0300

    initial commit

--- Grechkov_Stas_2/lab_2.c
+++ Grechkov_Stas_2/lab_2.c
@@ -1,55 +1,44 @@
 #include <stdio.h>
-void main()
-{ 
-	int n,index; // входные параметры
-	scanf("%d %d",&n,&index);
-	int mas[n];
-	int i;
-	for (i=0;i<n;i++)
-	{
-		scanf("%d",mas[i]); // заполнение массива
-	}
-	int max=0, count=0, sum=0,min=0; // max-максимальный элемент count-число повторов нулевого символа
-	                         // sum- cумма четных элементов min- минимальный элемент
-	switch (index)
-	{
-		case 0:
-	   {   for(i=0;i<n;i++)
-		{
-           if (mas[i]>max)
-           {
-           	max = mas[i];
-           }
-		} printf("%d \n",max);
-       }
-        case 1:
-       {
-          for(i=0;i<n;i++)
-         {
-           if (mas[i]<min || min ==0)
-           {
-           	min = mas[i];
-           }
-		} printf("%d \n",min);
-       }
-        case 2:
-       {   for(i=0;i<n;i++)
-		{
-           if (mas[i] % 2 ==0)
-           {
-           	sum = sum+mas[i];
-           }
-		} printf("%d \n",sum);
-       } 
-        case 3:
-       {   for(i=1;i<n;i++)
-		{
-           if (mas[i] == mas[0])
-           {
-           	count++;
-           }
-		} printf("%d \n",count);
-       }  
-       default: printf("Данные некорректны");
-	}
-} 
+int get_max(int arr[],int n)
+{
+ int max=0,i;
+  for (i=0;i<n;i++)
+  {
+   if (arr[i]>max) {max=arr[i];}
+  }return max;
+}
+int get_min(int arr[], int n)
+{
+ int min=0,i;
+ for(i=0,i<n;i++)
+  {
+   if (min>arr[i] || min==0) {min=arr[i];}
+  }return min;
+
+}
+int get_sum(int arr[],int n)
+{
+    int sum=0,i;
+  for(i=0;i<n;i++)
+   {
+    if ((arr[i] % 2) ==0 ) {sum+=arr[i];}
+   }return sum;
+}
+int get_count_first_el(int arr[], int n)
+{
+   int i,count=0;  
+  for(i=1;i<n;i++)
+  {
+    if (arr[i]==arr[0]) {count++;}
+  }return count;
+}
+  void menu(int index,int arr[],int n)
+{
+   switch(index)
+   case 0: printf("%d",get_max(arr[],n));
+   case 1: printf("%d",get_min(arr[],n));
+   case 2: printf("%d",get_sum(arr[],n));
+   case 3: printf("%d",get_count_first(arr[],n));
+   default: printf("");
+}
+
