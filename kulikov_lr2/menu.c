3c8df4ccb6d0e6f0b67a6415ba62009ffd332e96
RamonaFlow3rs <maximko98@gmail.com>
2016-10-07 13:47:25 +0300

    Update menu.c

--- kulikov_lr2/menu.c
+++ kulikov_lr2/menu.c
@@ -1,48 +1,53 @@
-#include <stdio.h>
-#include <math.h>
+int get_max(int arr[], int n){ 
+int max, i; 
+max = arr[0]; 
+for(i = 0; i<n; i++) 
+    if (max<arr[i]) 
+    max = arr[i]; 
+return max; 
+} 
 
-int main(){
-	int n = 0;
-	int wtd = 0;
-	int i = 0;
-	int big = 0;
-	int small = 0;
-	int sum = 0;
-	int count = 0;
-	scanf("%d\n %d\n",n, wtd);
-	int a[n];
-	if (wtd = 0) {
-		for (i = 0; i < n; ++i ){
-			if (a[i] > big) {
-				big = a[i];
-			}
-		}
-		printf("%d\n", big);
-	}
-	if (wtd = 1){
-		small = a[1];
-		for (i = 0; i < n; ++i){
-			if (a[i] < small){
-				small = a[i];
-			}
-		}
-		printf("%d\n",small);
-	}
-	if (wtd = 2){
-		for (int i = 0; i < n; ++i){
-			if ((fmod(a[i],2)) = 0){
-				sum = sum + a[i];
-			}
-		}
-		printf("%d\n", sum);
-	}
-	if (wtd = 3){
-		for (int i = 0; i < n; ++i){
-			if (a[i] = a[0]){
-				count = count + 1; 
-			}	
-		}
-		printf("%d\n", count);
-	}
+int get_min(int arr[], int n){ 
+int min,i; 
+min=arr[0]; 
+for(i = 0; i<n; i++) if (min>arr[i]) min = arr[i]; 
+return min; 
+} 
 
+int get_sum(int arr[], int n){ 
+int summ,i; 
+summ=0; 
+for(i = 0; i<n; i++) if (((arr[i])%2) == 0) summ+=arr[i]; 
+return summ; 
+} 
+
+int get_count_first_el(int arr[], int n){ 
+int k,i; 
+k = 0; 
+for(i = 1; i<n; i++) if (arr[0] == arr[i]) k++; 
+return k; 
+} 
+void menu(int ch, int arr[], int n){ 
+
+switch(ch){ 
+
+case 0: 
+printf("%d", get_max(arr, n)); 
+break; 
+
+case 1: 
+printf("%d", get_min(arr, n)); 
+break; 
+
+case 2: 
+printf("%d", get_sum(arr, n)); 
+break; 
+
+case 3: 
+printf("%d", get_count_first_el(arr, n)); 
+break; 
+
+default: 
+printf("Данные некорректны"); 
+    } 
 }
