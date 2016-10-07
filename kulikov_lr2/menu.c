c6d7adfe2a73c8f8b75c65fe6d9f57ca94532362
RamonaFlow3rs <maximko98@gmail.com>
2016-10-07 18:18:27 +0300

    Update menu.c

--- kulikov_lr2/menu.c
+++ kulikov_lr2/menu.c
@@ -1,40 +1,40 @@
 int get_max(int arr[], int n){ 
-int max, i; 
-max = arr[0]; 
-for(i = 0; i<n; i++) 
-    if (max<arr[i]) 
-    max = arr[i]; 
+    int max, i; 
+    max = arr[0]; 
+        for(i = 0; i<n; i++) 
+            if (max<arr[i]) 
+            max = arr[i]; 
 return max; 
 } 
 
 int get_min(int arr[], int n){ 
-int min,i; 
-min = arr[0]; 
-for(i = 0; i<n; i++) 
-    if (min>arr[i]) 
-        min = arr[i]; 
+    int min,i; 
+    min = arr[0];   
+        for(i = 0; i<n; i++) 
+            if (min>arr[i]) 
+            min = arr[i]; 
 return min; 
 } 
 
 int get_sum(int arr[], int n){ 
-int sum,i; 
-sum = 0; 
-for(i = 0; i<n; i++) 
-    if (((arr[i])%2) == 0) 
+    int sum,i; 
+    sum = 0; 
+    for(i = 0; i<n; i++) 
+        if (((arr[i])%2) == 0) 
         sum += arr[i]; 
 return sum; 
 } 
 
 int get_count_first_el(int arr[], int n){ 
-int count, i; 
-count = 0; 
-for(i = 1; i<n; i++) 
-    if (arr[0] == arr[i]) 
-        count++; 
+    int count, i; 
+    count = 0;  
+        for(i = 1; i<n; i++) 
+            if (arr[0] == arr[i]) 
+            count++; 
 return count; 
 } 
-void menu(int ch, int arr[], int n){ 
 
+void menu(int ch, int arr[], int n){ 
 switch(ch){ 
 
 case 0: 
