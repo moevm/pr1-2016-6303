1adac2b602ccf402e9ac6bed48c826d195a38142
filippinna1998 <fildasha1998@gmail.com>
2016-10-19 20:04:53 +0300

    a

---
+++ menu.c
@@ -0,0 +1,20 @@
+@linux:~/pr1-2016-6303$ nano menu.c
+  GNU nano 2.2.6               File: menu.c                           Modified
+
+    get_max(arr, n);
+    break;
+case 1 :
+    get_min(arr, n);
+    break;
+case 2 :
+    get_sum(arr, n);
+    break;
+case 3 :
+    get_count_first_el(arr, n);
+    break;
+default :
+    printf("Данные некорректны");
+}
+}
+
+
