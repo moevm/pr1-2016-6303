be46660f8e3584d0eeb7843755bea8eb9d3798b3
Kostya <foksen98@mail.ru>
2016-11-08 10:14:30 +0300

    Lab3

---
+++ Lab3/main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int check(char *sent);
+
+void sentence(int *control, int *before, int *quest);
+
+int main()
+{
+
+	int control = 0, before = 0, quest = 0;
+
+	while (control == 0)
+		sentence(&control, &before, &quest);
+
+  printf("Количество предложений до %i и количество предложений после %i\n", before, (before - quest));
+
+	return 0;
+}
+
+int check(char *sent)
+{
+	int j;
+	char str[] = "Dragon flew away!";
+
+	for (j = 0; j < 16; ++j)
+		if (*(sent + j) != str[j])
+			return 0;
+	return 1;
+}
+
+void sentence(int *control, int *before, int *quest)
+{
+	char sent[100];
+	char c;
+	int i = 0;
+
+	while (((c = getchar()) != '.') && (c != ';') && (c != '?'))
+		if (((c != '\t') && (c != '\n') && (c != ' ')) || (i != 0))
+		{
+			sent[i] = c;
+			if (i == 16)
+				if (check(sent) == 1)
+				{
+					*control = 1;
+					break;
+				}
+			++i;
+		}
+	if (*control == 0)
+	{
+		++*before;
+		if ((c == ';') || (c == '.'))
+			{
+				sent[i++] = c;
+				sent[i] = (char)0;
+				printf("%s\n", sent);
+			}
+		else
+			++*quest;
+	}
+}
