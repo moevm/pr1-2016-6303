20309e407cf3333e7a499f6628738a1aadbb27fd
RamonaFlow3rs <maximko98@gmail.com>
2016-11-10 21:40:36 +0300

    Update lab3.c

--- kulikov_lr3/lab3.c
+++ kulikov_lr3/lab3.c
@@ -1,47 +1,46 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h> 
 
-int main() { 
-char *str; 
-char check[] = "Dragon flew away!"; 
-int n = 0, m = 0, size = 10, len = 0, ch; 
-str = realloc(NULL, sizeof(char)*size); 
-	while(strstr(str,check) == NULL) { 
-		do { 
-			ch = getchar(); 
-		} 
+int main() {
+char *str;
+char check[] = "Dragon flew away!";
+int n = 0, m = 0, size = 10, len = 0, ch;
+str = realloc(NULL, sizeof(char)*size);
+	while(strstr(str,check) == NULL) {
+		do {
+			ch = getchar();
+		}
 	while (ch == 9 || ch == 32 || ch == 10);
-		str[len++] = ch; 
-	while((ch = getchar()) != EOF && ch != '.' && ch != ';' && ch != '?') { 
-	str[len++] = ch; 
-		if (len == size) { 
+		str[len++] = ch;
+	while((ch = getchar()) != EOF && ch != '.' && ch != ';' && ch != '?') {
+	str[len++] = ch;
+		if (len == size) {
 			str = realloc(str, sizeof(char)*(size += 16)); 
-			} 
-		} 
-str[len++] = '\0'; 
-len = 0; 
-switch((int)ch) { 
+			}
+		}
+str[len++] = '\0';
+len = 0;
+switch((int)ch) {
 	
 	case 63:
 		n++;
 		m++;
-		break; 
+		break;
 	
 	case 59:
 		printf("%s;\n", str);
 		n++;
-		break; 
+		break;
 	
 	case 46:
 		printf("%s.\n", str);
 		n++;
-		break; 
+		break;
 	
 	default:
-		break; 
-	} 
-} 
-printf("Количество предложений до %d и количество предложений после %d", n, n - m); 
-
+		break;
+	}
+}
+printf("Количество предложений до %d и количество предложений после %d", n, n - m);
 }
