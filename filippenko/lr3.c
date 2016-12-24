1ae768354b9b3a83bebc229b7c8296d2c20cfc9c
filippinna1998 <fildasha1998@gmail.com>
2016-12-24 20:03:29 +0300

    Update and rename lr3.c to filippenko/lr3.c

--- filippenko/lr3.c
+++ filippenko/lr3.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void read(int *a, int *after, int *question)
+void read(int *temp, int *after, int *question)
 {
-  char ch;
+  char symbol;
   int i = 0;
   char* arr = (char*)malloc(100 * sizeof(char));
-     while (((ch = getchar()) != '.') && (ch != '?') && (ch != ';'))
+     while (((symbol = getchar()) != '.') && (symbol != '?') && (symbol != ';'))
      {
-        if (ch == '!')
+        if (symbol == '!')
         {
-        *a = 1;
+        *temp = 1;
         break;
         }
-     if (((ch != '\t') && (ch != '\n') && (ch != ' ')) || (i != 0))
-     arr[i++] = ch;
+     if (((symbol != '\t') && (symbol != '\n') && (symbol != ' ')) || (i != 0))
+     arr[i++] = symbol;
      }
        
-       if (*a == 0)
-       if ((ch == '.') || (ch == ';'))
+       if (*temp == 0)
+       if ((symbol == '.') || (symbol == ';'))
          {
-           arr[i++] = ch;
+           arr[i++] = symbol;
            arr[i] = '\0';
            printf("%s\n", arr);
             (*after)++;
@@ -31,11 +31,11 @@ void read(int *a, int *after, int *question)
  
 int main()
 {
-  int a = 0;
+  int temp = 0;
   int after = 0;
   int question = 0;
     while (a == 0)
-    read(&a, &after, &question);
+    read(&temp, &after, &question);
   printf("Количество предложений до %d и количество предложений после %d", (after + question), after);
 return 0;
 }
