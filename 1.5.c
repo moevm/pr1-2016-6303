db7cd8f9e541a1cd15c8e4286dbc2109b1b963d3
prosperevergreen <prosperevergreen@gmail.com>
2017-02-10 00:32:55 +0300

    Update 1.5.c

--- 1.5.c
+++ 1.5.c
@@ -5,17 +5,18 @@ int main()
 {
     int i,b,min,max;
     printf("size of array\n");
-    scanf("%d",b);
-    int a[100];
+    scanf("%d",&b);
+    int*a=(int*)malloc(sizeof(int)*b);
+    //int a[100];
     for(i=0;i<b;i++)
     {
         scanf("%d",&a[i]);
     }
-    a[b]=\0;
+    //a[b]='\0';
     min=a[0];
     max=a[0];
     
-    for(i=1;i<d;i++)
+    for(i=1;a[i]!=NULL;i++)
     {
         if(min>a[i]) 
          min=a[i];
