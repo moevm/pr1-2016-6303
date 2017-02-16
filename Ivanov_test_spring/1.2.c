1122f08be12ef07b787560c77a2494855bc4e390
IvanovDmitry99 <ivanov_dmitry_99@bk.ru>
2017-02-16 18:20:10 +0300

    Create 1.2.c

---
+++ Ivanov_test_spring/1.2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{	
+	char c;
+	int n=1;
+	c=getchar();
+	while (c!='\n')
+	{
+		if (n % 2 == 0)
+			printf("%c", c);
+		c=getchar();
+		n++;
+	}
+	printf("\n");
+    return 0;
+}
