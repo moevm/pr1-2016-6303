2a27dc0c1ce5a9da7049f5a682e81ad3092d25f1
IvanovDmitry99 <ivanov_dmitry_99@bk.ru>
2017-03-02 14:31:33 +0300

    2 semestr. Lab 1

---
+++ Ivanov_2_lr1/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h> 
+#include <time.h> 
+#include <stdlib.h> 
+#define lenght 1000
+
+int compare(const void *, const void *); //прототив функции compare(для функции сортировки qsort)
+
+int main() 
+{ 
+	int arr[lenght];
+	for (int i = 0; i < lenght; i++) 
+		scanf("%d", &arr[i]); 
+	time_t sort_start, sort_end; 
+	sort_start = time(NULL); 
+	qsort(arr, lenght, sizeof(int), compare);	//сортировка посредством функции qsort)
+	sort_end = time(NULL); 
+	for (int i = 0; i < lenght; i++) 
+		printf("%d ", arr[i]); 
+	printf("\n%f", difftime(sort_end, sort_start));		//вывод времени сортировки (разность между временем начала sort_start и временем окончания sort_end)
+	return 0; 
+} 
+
+
+int compare(const void * a, const void * b) 
+{ 
+	return (abs(*(int*)b) - abs(*(int*)a)); 
+}
