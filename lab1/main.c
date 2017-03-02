9f7563440b5430df9406aaea8ff1459433910d45
jerihon <jerihon1@gmail.com>
2017-03-02 16:41:04 +0300

    lab1_sem2

--- lab1/main.c
+++ lab1/main.c
@@ -1,54 +1,45 @@
-#define N 10
-
 #include <stdio.h>
 #include <stdlib.h> 
 #include <time.h>
+#define N 1000
 
-int cmpval (const void * a, const void * b){ 
+
+int cmpval (const void * a, const void * b) {  //функция сравнения двух элементов массива
     return (*(int*)a-*(int*)b);
-} 
+}
+
 
 int main() {
     int arr[N];
-     clock_t start, stop, timeB, timeQ;
-   
-
-  for (int i = 0; i < N; i++) 
-  {
+    clock_t start, stop, timeB, timeQ;
+    for (int i = 0; i < N; i++) {
     scanf("%d", &arr[i]);
-  }
+    }
 
-
- 
-  start = clock();  
-  for (int i = 0 ; i < N - 1; i++) { // сравнение двух соседних элементов 
-    for (int j = 0 ; j < N - i - 1 ; j++) {  
-           if(arr[j] > arr[j+1]) { // если они идут в неправильном порядке,
-            int tmp = arr[j];   // то меняем их местами
-              arr[j] = arr[j+1] ;
-              arr[j+1] = tmp;
+    start = clock();  
+        for (int i = 0 ; i < N - 1; i++) { // сравнение двух соседних элементов 
+            for (int j = 0 ; j < N - i - 1 ; j++) {  
+                if(arr[j] > arr[j+1]) { // если они идут в неправильном порядке,
+                    int tmp = arr[j];   // то меняем их местами
+                    arr[j] = arr[j+1] ;
+                    arr[j+1] = tmp;
           }
         }
-    }   // впадлу было вынести в функцию, да
+    }   
   stop = clock();
   timeB = stop - start;
 
- 
-
- 
   start = clock();
-  qsort(arr, N, sizeof(int), cmpval); //пизж со Славки
+  qsort(arr, N, sizeof(int), cmpval);
   stop = clock();
   timeQ = stop - start;
  
-
-    for (int i = 0; i < N; i++) 
-  {
+  for (int i = 0; i < N; i++) {
     printf("%d ", arr[i]);
   }
-  printf("\n");
+ printf("\n");
+ 
  printf("BubbleSort time = %d\n", timeB);
  printf("QuickSort time = %d\n", timeQ);
-  return 0;
-
+ return 0;
 }
