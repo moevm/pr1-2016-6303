844dab332f6c79be9531cef91275e94826a627a3
Danya <deadlyshine@gmail.com>
2017-03-06 10:17:00 +0300

    upd: add bsearch and any fixes

--- Lab_1.c
+++ Lab_1.c
@@ -1,81 +1,59 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
 #include <time.h> // работа со временем
 
-#define S_NUMBER 0 // искомое число
 #define ARR_SIZE 1000 // для тестирования мне была необходима возможность установки меньшего размера массива в кратчайшие сроки
 
-int compare(const void* arg_1, const void* arg_2); // прототип функции сравнения чисел для qsort
+int compare(const void* arg_1, const void* arg_2); // прототип функции сравнения чисел для qsort и bsearch
 
 int main() {
-	short exist = 0; // принимает 1 или 0 в зависимости от того существует ноль или нет
-	int low = 0; // нижняя граница при бинарном поиске
-	int high = ARR_SIZE-1; // верхняя граница при бинарном поиске
-	int count; // счетчик, в целом используется несколько раз в качестве счетчика в разных местах
+	int key = 0; // искомое число
 	int nums[ARR_SIZE]; // массив чисел
-	
+
 	for (int i = 0; i<ARR_SIZE; i++)
 	{
 		scanf("%d", &nums[i]); // цикл заполняющий массив числами с потока ввода
 	}
-	
+
 	qsort(nums, ARR_SIZE, sizeof(int), compare); // сортировка массива quicksort-ом
 
-	time_t time_start = time(NULL); // запись времени начала поиска
+	clock_t t_active = clock(); // запись времени начала поиска
 
-	while (low != high)
-	{
-		count = (low + high) / 2; // если число больше искомого числа, значит мы находимся выше этого числа и это становится
-		if (nums[count] > S_NUMBER)	// верхней границей в дальнейшем поиске
-		{
-			high = count;
-			continue;
-		}
-		if (nums[count] < S_NUMBER) // меньше искомого числа - нижняя граница
-		{
-			low = count;
-			continue;
-		}
-		if (nums[count] == S_NUMBER) // если число равно искомому, то exist принимает значение "1"
-		{			//что эквивалентно "ноль существует" в логике программы
-			exist = 1;
-			break;
-		}
-	}
+	int * ptrItem = (int*)bsearch(&key, nums, 1000, sizeof(int), compare);
 
-	time_t time_finish = time(NULL); // запись времени окончания поиска
+	t_active = clock() - t_active; // запись времени окончания поиска
 
-	if (exist == 1)
+	if (ptrItem != NULL)
 		printf("exists\n"); // вывод exists если флаг поднят (ноль найден) и doesn't exist если нет
-	else 
+	else
 		printf("doesn't exist\n");
-	exist = 0; // установка флага равным нулю, для дальнейшего его использования в повторном поиске
+	ptrItem = NULL;
 
-	printf("%f\n", difftime(time_finish, time_start)); // вывод разницы между временем начала и конца поиска
+	printf("%f\n", (double)t_active / CLOCKS_PER_SEC); // вывод разницы между временем начала и конца поиска
 
-	time_start = time(NULL); // запись времени старта поиска
+	t_active = clock();
 
-	for (count = 0; count<ARR_SIZE; count++) // поиск пузырьком
+	for (int i = 0; i<ARR_SIZE; i++) // поиск пузырьком
 	{
-		if (nums[count] == S_NUMBER)
+		if (nums[i] == key)
 		{
-			exist = 1; // установка флага 1 если ноль найден
+			ptrItem = &key; // установка флага 1 если ноль найден
 			break;
 		}
 	}
 
-	time_finish = time(NULL); // запись времени окончания поиска
+	t_active = clock() - t_active; // запись времени окончания поиска
 
-	if (exist == 1) // вывод exists если флаг поднят (ноль найден) и doesn't exist если нет
-		printf("exists\n");
+	if (ptrItem != NULL)
+		printf("exists\n"); // вывод exists если флаг поднят (ноль найден) и doesn't exist если нет
 	else
 		printf("doesn't exist\n");
 
-	printf("%f\n", difftime(time_finish, time_start)); // вывод времени поиска пузырьком
+	printf("%f\n", (double)t_active / CLOCKS_PER_SEC); // вывод времени поиска пузырьком
 	return 0;
 }
 
 int compare(const void* arg_1, const void* arg_2)
 {
-	return (*(int*)arg_1 - *(int*)arg_2); // функция сравнивающая числа, используется для qsort-а
+	return (*(int*)arg_1 - *(int*)arg_2); // функция сравнивающая числа, используется для qsort и bsearch
 }
