0b9c5cfc52d3d236a065d9497025ff9300a4b057
Kostya <foksen98@mail.ru>
2017-03-08 23:09:29 +0300

    Fokin LR №1 done(updated code)

--- fokin_sem2_lr1/main_lr1.c
+++ fokin_sem2_lr1/main_lr1.c
@@ -2,14 +2,9 @@
 #include "stdlib.h"
 #include "string.h"
 
-int comparator_qsort (const void* x1, const void* x2)
+int comparator (const void* x1, const void* x2)
 {
     return strcmp(*((char**)x1), *((char**)x2));
-}
-
-int comparator_bsearch (const void* x1, const void* x2)
-{
-    return strcmp((char*)x1, *((char**)x2));
 }
 
 int main()
@@ -19,14 +14,15 @@ int main()
     char str[32];
     char* arr_ptr[1000];
     char* ptr = NULL;
-    char* ptr_str = NULL;
+    char* ptr_bsearch = NULL;
+    char* str_ptr = NULL;
 
     fgets(arr, sizeof(arr), stdin);
-    if (strstr(arr, "\n"))
-        *(strstr(arr, "\n")) = '\0';
+    *(strstr(arr, "\n")) = '\0';
     fgets(str, sizeof(str), stdin);
     if (strstr(str, "\n"))
         *(strstr(str, "\n")) = '\0';
+    str_ptr = str;
 
     ptr = strtok(arr, " .");
     while (ptr)
@@ -35,9 +31,9 @@ int main()
         ptr = strtok(NULL, " .");
     }
 
-    qsort(arr_ptr, i, sizeof(char*), comparator_qsort);
-    ptr_str = (char*)bsearch(str, arr_ptr, i, sizeof(char*), comparator_bsearch);
-    if (ptr_str)
+    qsort(arr_ptr, i, sizeof(char*), comparator);
+    ptr_bsearch = (char*)bsearch(&str_ptr, arr_ptr, i, sizeof(char*), comparator);
+    if (ptr_bsearch)
         printf("exists\n");
     else
        printf("doesn't exist\n");
