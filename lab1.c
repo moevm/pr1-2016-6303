bb52e1079332b1963b89f83334db04cee45fe15f
justaleaf <nik.vag@mail.ru>
2017-03-09 10:00:36 +0300

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -2,16 +2,23 @@
 #include <time.h> 
 #include <stdlib.h> 
 
-int compare(const void * a, const void * b); 
+int compare(const void * x1, const void * x2) 
+{ 
+return (*(int*)x1 - *(int*)x2); 
+}
 
 int main() 
 { 
-int numbers[1000]; 
+int numbers[1000],numberscopy[1000]; 
 int i=0,j,len=1000; 
-for (i; i < len; i++) 
+for (i; i < len; i++)
+{
 scanf("%d", &numbers[i]); 
-time_t sort_start, sort_end, qsort_start, qsort_end; 
-sort_start = time(NULL); 
+}
+memcpy(numberscopy, numbers, 1000 * sizeof (int));  //копируем исходный массив, дабы провести две разных сортировки
+clock_t bubblesort_start, bubblesort_end, qsort_start, qsort_end; 
+/* сортировка пузырьком (bubble) */
+bubblesort_start = clock(); 
 for (i = 0; i < (len-1); i++) 
 {
     for (j = 0; j < (len - i - 1); j++) 
@@ -24,19 +31,18 @@ for (i = 0; i < (len-1); i++)
         }
     }
 }
-sort_end = time(NULL); 
-qsort_start = time(NULL); 
-qsort(numbers, len, sizeof(int), compare); 
-qsort_end = time(NULL); 
-for (i = 0; i < len; i++) printf("%d ", numbers[i]); 
+bubblesort_end = clock();
+/* Сортировка с помощью qsort */   
+qsort_start = clock(); 
+qsort(numberscopy, len, sizeof (int), compare); 
+qsort_end = clock(); 
+    
+for (i = 0; i < len; i++)
+{
+    printf("%d ", numbers[i]); 
+}
 printf("\n"); 
-printf("%f\n", difftime(sort_end, sort_start)); 
-printf("%f", difftime(qsort_end, qsort_start)); 
+printf("%f\n", (float)(bubblesort_end - bubblesort_start) / CLOCKS_PER_SEC); 
+printf("%f\n", (float)(qsort_end - qsort_start) / CLOCKS_PER_SEC); 
 return 0; 
 } 
-
-
-int compare(const void * a, const void * b) 
-{ 
-return (*(int*)a - *(int*)b); 
-}
