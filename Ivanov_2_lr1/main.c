c6a59cd16605b23109b2cb621c2d6dfd459df33d
IvanovDmitry99 <ivanov_dmitry_99@bk.ru>
2017-03-09 14:55:50 +0300

    2 semestr. Lab 1

--- Ivanov_2_lr1/main.c
+++ Ivanov_2_lr1/main.c
@@ -1,27 +1,28 @@
 #include <stdio.h> 
 #include <time.h> 
 #include <stdlib.h> 
-#define lenght 1000
+#define LenghtOfArray 1000
 
-int compare(const void *, const void *);	//прототив функции compare(для функции сортировки qsort)
+int compare(const void *, const void *); 
 
 int main() 
 { 
-	int arr[lenght];
-	for (int i = 0; i < lenght; i++) 
-		scanf("%d", &arr[i]); 
-	time_t sort_start, sort_end; 
-	sort_start = time(NULL); 
-	qsort(arr, lenght, sizeof(int), compare);	//сортировка посредством функции qsort)
-	sort_end = time(NULL); 
-	for (int i = 0; i < lenght; i++) 
-		printf("%d ", arr[i]); 
-	printf("\n%f", difftime(sort_end, sort_start));		//вывод времени сортировки (разность между временем начала sort_start и временем окончания sort_end)
-	return 0; 
+	int ArrayOfNumbers[LenghtOfArray];
+	for (int i = 0; i < LenghtOfArray; i++) 
+	scanf("%d", &ArrayOfNumbers[i]); 
+	time_t time_of_start, time_of_end; 
+	time_of_start = time(NULL); 
+	qsort(ArrayOfNumbers, LenghtOfArray, sizeof(int), compare); 
+	time_of_end = time(NULL); 
+	for (int i = 0; i < LenghtOfArray; i++) 
+		printf("%d ", ArrayOfNumbers[i]); 
+	printf("\n%f", difftime(time_of_end, time_of_start)); 
+return 0; 
 } 
 
 
-int compare(const void *a, const void *b) 
+int compare(const void * a, const void * b) 
 { 
 	return (abs(*(int*)b) - abs(*(int*)a)); 
 }
+
