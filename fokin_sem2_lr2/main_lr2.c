10250913767abb7773396c712fe824818d3c056e
Kostya <foksen98@mail.ru>
2017-04-13 14:25:11 +0300

    Fokin LR №2 done

--- fokin_sem2_lr2/main_lr2.c
+++ fokin_sem2_lr2/main_lr2.c
@@ -1,28 +1,55 @@
 /*
     Программа main_lr2.c (Л/р №2) определяет валидность кода html-страницы.
-    Необходима проверка на наличие закрывающей скобки (">") в теге
-    (в условии не сказано, но иначе "Failed test #22. Runtime error:").
 
-    By foksen98 02.04.2017.
+    By foksen98 02.04.2017 (Last update 13.04.2017).
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 //  максимальная длина входной строки
-#define LENGTH 500
+#define LENGTH 3000
 
 //  структура элемента списка
-typedef struct bracket
+typedef struct stack
 {
     char* tag;
-    struct bracket* prev;
-} bracket;
+    struct stack* prev;
+} stack;
+
+//  функция добавляет в список структуру с именем последнего открывающего
+stack* push (char* tag, stack* current)
+{
+    stack* next = (stack*)malloc(sizeof(stack));
+    next->tag = tag;
+    next->prev = current;
+    return next;
+}
+
+//  функция удаляет последний элемент списка
+stack* pop (stack* current)
+{
+    stack* temp = current->prev;
+    free(current);
+    return temp;
+}
+
+//  функция извлекает значение последнего элемента списка
+char* top (stack* current)
+{
+    return current->tag;
+}
+
+//  функция проверяет список на пустоту
+stack* isEmpty (stack* current)
+{
+    return current->prev;
+}
 
 //  функция освобождает динамическую память (весь список)
-void delete (bracket* current)
+void delete (stack* current)
 {
-    bracket* temp;
+    stack* temp;
     while (current->prev)
     {
         temp = current->prev;
@@ -32,89 +59,38 @@ void delete (bracket* current)
     free(current);
 }
 
-//  функция добавляет в список структуру с именем последнего открывающего
-//  (но требующего закрытия) тега
-bracket* add (char* tag, bracket* current)
-{
-    //  если тег не требует закрытия, возвращаем указатель на текущий
-    //  (последний) элемент списка
-    if (!strcmp(tag, "br") || !strcmp(tag, "hr"))
-        return current;
-    //  иначе создаем новый элемент списка с именем последнего открывающего
-    //  тега и возвращаем указатель на него
-    else
-    {
-        bracket* next = (bracket*)malloc(sizeof(bracket));
-        next->tag = tag;
-        next->prev = current;
-        return next;
-    }
-}
-
-//  функция проверяет, закрывает ли этот тег последний отрывающий тег из списка
-bracket* check (char* tag, bracket* current)
-{
-    //  если имя закрывающего тега соответствует имени последнего
-    //  открывающего тега, то удаляем последний элеменент списка и
-    //  возвращаем указатель на предпоследний
-    if (!strcmp(tag, current->tag))
-    {
-        bracket* temp = current->prev;
-        free(current);
-        return temp;
-    }
-    //  иначе освобождаем память всего списка и возвращаем NULL
-    else
-    {
-        delete(current);
-        return NULL;
-    }
-}
-
 int main()
 {
     char str[LENGTH + 2];
-    char* ptr;
-    char* temp;
-    bracket* current = (bracket*)malloc(sizeof(bracket));
+    char *ptrOpen, *ptrClose, *tag;
+    stack* current = (stack*)malloc(sizeof(stack));
     current->tag = "null";
     current->prev = NULL;
 
     fgets(str, (LENGTH + 2), stdin);
 
-    ptr = strstr(str, "<");
-    //  проверка на наличие закрывающей скобки для открывающей
-    //  (в данном случае строка "<tag1" считается валидной)
-    if (!strstr(str, ">"))
-        ptr = NULL;
+    ptrOpen = strstr(str, "<");
+    ptrClose = strstr(str, ">");
 
-    while (ptr)
+    while (ptrOpen)
     {
-        //  если тег закрывающий, то вызываем функцию check() для проверки
-        if (*(ptr + 1) == '/')
-        {
-            temp = (strstr(ptr, ">") + 1);
-            current = check((strtok(ptr, ">") + 2), current);
-            //  если функция check() вернула NULL, выводим "wrong"
-            //  и завершаем программу
-            if (!current)
+        if (*(ptrOpen + 1) == '/')
+            if (!strncmp((ptrOpen + 2), top(current), (ptrClose - ptrOpen - 2)))
+                current = pop(current);
+            else
             {
+                delete(current);
                 printf("wrong\n");
                 return 0;
             }
-        }
-        //  иначе вызываем функцию add() для добавления нового открывающего
-        //  тега в список
         else
         {
-            temp = (strstr(ptr, ">") + 1);
-            current = add((strtok(ptr, ">") + 1), current);
+            tag = strtok((ptrOpen + 1), ">");
+            if (strcmp(tag, "br") && strcmp(tag, "hr"))
+                current = push(tag, current);
         }
-        ptr = strstr(temp, "<");
-        //  проверка на наличие закрывающей скобки для открывающей
-        //  (строка "<tag1><tag2></tag2></tag1" считается невалидной)
-        if (!strstr(temp, ">"))
-            ptr = NULL;
+        ptrOpen = strstr((ptrClose + 1), "<");
+        ptrClose = strstr((ptrClose + 1), ">");
     }
 
     //  если были закрыты все открывающие теги (осталась только голова списка),
