92840095263d6831dcbf175cf5d9307e030e41bc
IvanovDmitry99 <ivanov_dmitry_99@bk.ru>
2017-05-19 15:14:34 +0300

    Kurs_Rab

--- Ivanov_KursRab_2sem/main.c
+++ Ivanov_KursRab_2sem/main.c
@@ -1,5 +1,13 @@
-//РРІР°РЅРѕРІ Р”РјРёС‚СЂРёР№ Р’Р»Р°РґРёРјРёСЂРѕРІРёС‡. Р“СЂСѓРїРїР° 6303
-//РљРѕРґ РљСѓСЂСЃРѕРІРѕР№ СЂР°Р±РѕС‚С‹
+/*
+
+Все ситуации, за исключением нормальной:
++ В одном из файлов больше студентов, чем во втором                                     - провека кол-во строк
++ Отсутствие: в 1ом - Фамилия/Имя/Отчество/Гит/Мэйл/Группа, во 2ом - Фамилия/Имя/Балл!  - строковый валидатор
++ Студента из 2ого нет в 1ом (в случае одинакового кол-ва студентов в файлах)           - поиск из второго в первом
++ Больше одного студента имеют одно имя-фамилия в 1ом(+), во 2ом(+)                     - подсчёт кол-ва одинаковых фи/проверка массива имён
++ В поле балл не балл																	- супер-условие из лабы для atoi
+
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,16 +15,22 @@
 #include <locale.h>
 #define MAX_NUM 1000
 
-typedef struct Students
+typedef struct StudentsF1
 {
-    char Surname[30];
-    char Name[30];
-    int Score;
-} Students;
+	char Surname[30];
+	char Name[30];
+} StudentsF1;
 
-//РџСЂРѕРІРµСЂСЏРµС‚ РІР°Р»РёРґРЅРѕСЃС‚СЊ СЃС‚СЂРѕРєРё.
-//РџСЂРёРЅРёРјР°РµС‚ СЃС‚СЂРѕРєСѓ Рё ID С„Р°Р№Р»Р°, РёР· РєРѕС‚РѕСЂРѕРіРѕ Р±С‹Р»Р° СЃС‡РёС‚Р°РЅР° СЃС‚СЂРѕРєР°. РџРµСЂРІС‹Р№ С„Р°Р№Р» ID=6, РІС‚РѕСЂРѕР№ - ID=3
-//Р’РѕР·РІСЂР°С‰Р°РµС‚ РєРѕРґ РѕС€РёР±РєРё.
+typedef struct StudentsF2
+{
+	char Surname[30];
+	char Name[30];
+	int Score;
+} StudentsF2;
+
+//Проверяет валидность строки.
+//Принимает строку и ID файла, из которого была считана строка. Первый файл ID=6, второй - ID=3
+//Возвращает код ошибки.
 int ValidityOfString(char *string, int ID)
 {
 	char tmp[200];
@@ -36,7 +50,7 @@ int ValidityOfString(char *string, int ID)
 		if ((ID == 3) && (count == 3))
 		{
 			int score = atoi(tmp);
-			if ((score == 0) && (strcmp(tmp,"0")!=0))
+			if ((score == 0) && (strcmp(tmp, "0") != 0))
 				return 3;
 		}
 		count++;
@@ -48,17 +62,17 @@ int ValidityOfString(char *string, int ID)
 		return 0;
 }
 
-//РћРїСЂРµРґРµР»СЏРµС‚ С‚РёРї РїР°СЂР°РјРµС‚СЂР° РѕС€РёР±РєРё Рё РїРµС‡Р°С‚Р°РµС‚ СЃРѕРѕР±С‰РµРЅРёРµ.
-//РџСЂРёРЅРёРјР°РµС‚ РєРѕРґ РѕС€РёР±РєРё Рё ID С„Р°Р№Р»Р°, РёР· РєРѕС‚РѕСЂРѕРіРѕ Р±С‹Р»Р° СЃС‡РёС‚Р°РЅР° СЃС‚СЂРѕРєР°
-//Р’РѕР·РІСЂР°С‰Р°РµС‚ 0, РµСЃР»Рё РѕС€РёР±РєРё РЅРµС‚, 1 - РµСЃР»Рё РѕС€РёР±РєР° РµСЃС‚СЊ
+//Определяет тип параметра ошибки и печатает сообщение.
+//Принимает код ошибки и ID файла, из которого была считана строка
+//Возвращает 0, если ошибки нет, 1 - если ошибка есть
 int ParametrOfError(int ErrorCode, int ID)
 {
 	switch (ErrorCode)
 	{
 	case 0: return 0;
-	case 1: if (ID == 6) printf("Failed with <Surname. File 1>\n"); else printf("Failed with <Surname. File 2>\n"); break;
-	case 2: if (ID == 6) printf("Failed with <Name. File 1>\n"); else printf("Failed with <Name. File 2>\n"); break;
-	case 3: if (ID == 6) return 0; else printf("Failed with <Score>\n"); break;
+	case 1: if (ID == 6) printf("Failed with <Surname. File №1>\n"); else printf("Failed with <Surname. File №2>\n"); break;
+	case 2: if (ID == 6) printf("Failed with <Name. File №1>\n"); else printf("Failed with <Name. File №2>\n"); break;
+	case 3: if (ID == 6) return 0; else printf("Failed with <Score>\n"); break;							//Отчество может отсутствовать
 	case 4: printf("Failed with <Git>\n"); break;
 	case 5: printf("Failed with <Mail>\n"); break;
 	case 6: printf("Failed with <Group>\n"); break;
@@ -66,76 +80,96 @@ int ParametrOfError(int ErrorCode, int ID)
 	return -1;
 }
 
-//РЎРѕС…СЂР°РЅСЏРµС‚ Р¤Р+Р±Р°Р»Р» СЃС‚СѓРґРµРЅС‚Р° РІ РјР°СЃСЃРёРІ Рё РїСЂРѕРІРµСЂСЏРµС‚ РґСѓР±Р»РёСЂРѕРІР°РЅРёРµ
-//РџСЂРёРЅРёРјР°РµС‚ СЃС‚СЂРѕРєСѓ РёР· 2РѕРіРѕ С„Р°Р№Р»Р°, СѓРєР°Р·Р°С‚РµР»СЊ РЅР° РјР°СЃСЃРёРІ СЃС‚СѓРґРµРЅС‚РѕРІ Рё РЅРѕРјРµСЂ СЃР»РµРґ. СЃС‚СѓРґРµРЅС‚Р°
-//Р’РѕР·РІСЂР°С‰Р°РµС‚ -1 РІ СЃР»СѓС‡Р°Рµ, РµСЃР»Рё СЃС‚СѓРґРµРЅС‚ СЃ С‚Р°РєРёРј Р¤Р СѓР¶Рµ Р±С‹Р» РЅР°Р№РґРµРЅ СЂР°РЅРµРµ (РїРѕРІС‚РѕСЂ), 0 - РµСЃР»Рё РІСЃРµ Ok
-int SaveStud (char *string, Students *StudFromSec, int num)
+//Сохраняет ФИ+балл студента из первого файла в массив и проверяет дублирование
+//Принимает строку из 2ого файла, указатель на массив студентов и номер след. студента
+//Возвращает -1 в случае, если студент с таким ФИ уже был найден ранее (повтор), 0 - если все Ok
+int SaveStudF1(char *string, StudentsF1 *StudFromFir, int num)
 {
-    char *Surname = strtok(string, ";,");
+	char *Surname = strtok(string, ";,");
+	char *Name = strtok(NULL, ";,");
+	if (num>1)
+		for (int i = 0; i<num; i++)
+			if ((strcmp(Surname, StudFromFir[i].Surname) == 0) && (strcmp(Name, StudFromFir[i].Name) == 0))
+				return -1;
+
+	strcpy(StudFromFir[num].Surname, Surname);
+	strcpy(StudFromFir[num].Name, Name);
+	return 0;
+}
+
+//Сохраняет ФИ+балл студента из второго файла в массив и проверяет дублирование
+//Принимает строку из 2ого файла, указатель на массив студентов и номер след. студента
+//Возвращает -1 в случае, если студент с таким ФИ уже был найден ранее (повтор), 0 - если все Ok
+int SaveStudF2(char *string, StudentsF2 *StudFromSec, int num)
+{
+	char *Surname = strtok(string, ";,");
 	char *Name = strtok(NULL, ";,");
 	char *Score = strtok(NULL, ";,");
-	int i;
 	if (num>1)
-		for (i=0; i<num; i++)
+		for (int i = 0; i<num; i++)
 			if ((strcmp(Surname, StudFromSec[i].Surname) == 0) && (strcmp(Name, StudFromSec[i].Name) == 0))
 				return -1;
 
 	strcpy(StudFromSec[num].Surname, Surname);
 	strcpy(StudFromSec[num].Name, Name);
-	StudFromSec[num].Score =atoi(Score);
+	StudFromSec[num].Score = atoi(Score);
 	return 0;
 }
 
-//РС‰РµС‚ СЃС‚СѓРґРµРЅС‚Р° РёР· 2РіРѕ С„Р°Р№Р»Р° РІ 1РѕРј.
-//РџСЂРёРЅРёРјР°РµС‚ СѓРєР°Р·Р°С‚РµР»СЊ РЅР° РїРµСЂРІС‹Р№ С„Р°Р№Р» Рё СЃС‚СѓРґРµРЅС‚Р°.
-//Р’РѕР·РІСЂР°С‰Р°РµС‚ -2, РµСЃР»Рё РІ С„Р°Р№Р»Рµ РЅРµСЃРєРѕР»СЊРєРѕ СЃС‚СѓРґРµРЅС‚РѕРІ СЃ РѕРґРЅРёРј Р¤Р, -1, РµСЃР»Рё СЃС‚СѓРґРµРЅС‚ РЅРµ РЅР°Р№РґРµРЅ Рё Р±Р°Р»Р» СЃС‚СѓРґРµРЅС‚Р°, РµСЃР»Рё РІСЃС‘ Ok
-int Search(FILE *In1, Students StudFromSec)
+//Ищет студента из 2го файла в 1ом.
+//Принимает указатель на первый файл и студента.
+//Возвращает -2, если в файле несколько студентов с одним ФИ, -1, если студент не найден и балл студента, если всё Ok
+int Search(StudentsF1 *StudFromFir, StudentsF2 StudFromSec, int NumOfStud)
 {
 	char StringFromFirst[200];
-	int count = 0, score=0;
-
-	while (!feof(In1))
+	int count = 0, score = 0;
+	int i;
+	for (i = 0; i < NumOfStud; i++)
 	{
-		fscanf(In1, "%s", StringFromFirst);
-		if (StringFromFirst[0] != '\0')
-		{
-			char *Surname1 = strtok(StringFromFirst, ";,\n");
-			if (strcmp(Surname1, StudFromSec.Surname) == 0)
+//		printf("%s!=%s %s!=%s\n", StudFromFir[i].Surname, StudFromSec.Surname, StudFromFir[i].Name, StudFromSec.Name);
+		if (strcmp(StudFromFir[i].Surname, StudFromSec.Surname) == 0)
 			{
-				char *Name1 = strtok(NULL, ";,\n");
-				if (strcmp(Name1, StudFromSec.Name) == 0)
+				if (strcmp(StudFromFir[i].Name, StudFromSec.Name) == 0)
 				{
 					count++;
 				}
 			}
-		}
-		StringFromFirst[0] = '\0';
 	}
+
 	if (count > 1)
 		return -2;
 	else
 		if (count == 1)
-	        {
-	            return StudFromSec.Score;
-	        }
-		else return -1;
+		{
+			return StudFromSec.Score;
+		}
+		else
+		{
+			//printf("%s!=%s %s!=%s\n", StudFromFir[i].Surname, StudFromSec.Surname, StudFromFir[i].Name, StudFromSec.Name);
+			return -1;
+		}
 }
 
-//РџСЂРѕРІРµСЂСЏРµС‚ РІР°Р»РёРґРЅРѕСЃС‚СЊ С„Р°Р№Р»РѕРІ - РёС… СЃСѓС‰РµСЃС‚РІРѕРІР°РЅРёРµ, РІР°Р»РёРґРЅРѕСЃС‚СЊ СЃС‚СЂРѕРє, СЃРѕРґРµСЂР¶Р°С‰РёС…СЃСЏ РІ С„Р°Р№Р»Р°С…; Р° С‚Р°РєР¶Рµ СЃРѕС…СЂР°РЅСЏРµС‚ СЃС‚СѓРґРµРЅС‚РѕРІ РёР· 2РѕРіРѕ С„Р°Р№Р»Р°
-//РџСЂРёРЅРёРјР°РµС‚ СѓРєР°Р·Р°С‚РµР»Рё РЅР° С„Р°Р№Р»С‹ Рё РЅР° РјР°СЃСЃРёРІ СЃС‚СѓРґРµРЅС‚РѕРІ
-//Р’РѕР·РІСЂР°С‰Р°РµС‚ Р·РЅР°С‡РµРЅРёРµ РІ Р·Р°РІРёСЃРёРјРѕСЃС‚Рё РѕС‚ РѕС€РёР±РєРё: -4 - СЃС‡РёС‚Р°РЅРЅС‹Р№ СЃС‚СѓРґРµРЅС‚ СѓР¶Рµ Р±С‹Р» РЅР°Р№РґРµРЅ СЂР°РЅРµРµ
-//-3 - С„Р°Р№Р» РЅРµ СЃСѓС‰РµСЃС‚РІСѓРµС‚, -2 - СЃС‚СЂРѕРєР° РІ С„Р°Р№Р»Рµ РЅРµ РєРѕСЂСЂРµРєС‚РЅР°, -1 - РєРѕР»-РІРѕ СЃС‚СЂРѕРє РІ С„Р°Р№Р»Р°С… СЂР°Р·Р»РёС‡Р°РµС‚СЃСЏ
-//Р•СЃР»Рё РѕС€РёР±РѕРє РЅРµС‚, РІРѕР·РІСЂР°С‰Р°РµС‚ РєРѕР»-РІРѕ СЃС‚СЂРѕРє(СЃС‚СѓРґРµРЅС‚РѕРІ) РІ С„Р°Р№Р»Рµ
-int ValidOfFile(FILE *file1, FILE *file2, Students *StudFromSec)
+//Проверяет валидность файлов - их существование, валидность строк, содержащихся в файлах; а также сохраняет студентов из 2ого файла
+//Принимает указатели на файлы и на массив студентов
+//Возвращает значение в зависимости от ошибки: -4 - считанный студент уже был найден ранее
+// -7 - одинаковые студенты по 2ом, -6 - одинаковые студенты в 1ом, -5 - нет 2ого файла, -4 - нет 1ого файла, -3 - оба файла не существуют, -2 - строка в файле не корректна, -1 - кол-во строк в файлах различается
+//Если ошибок нет, возвращает кол-во строк(студентов) в файле
+int ValidOfFile(FILE *file1, FILE *file2, StudentsF1 *StudFromFir, StudentsF2 *StudFromSec)
 {
 	int i1 = 0, i2 = 0;
-	if ((file1 == NULL)||(file2 == NULL))
+	if ((file1 == NULL) || (file2 == NULL))
 	{
+		if ((file1 == NULL) && (file2 == NULL))
+			return -3;
 		if (file1 == NULL)
-        		printf("input_file_1.csv doesn't exist\n");
+		{
+			return -4;
+		}
 		if (file2 == NULL)
-		        printf("input_file_2.csv doesn't exist\n");
-		return -3;
+		{
+			return -5;
+		}
 	}
 
 	char *string = (char *)malloc(200);
@@ -144,10 +178,12 @@ int ValidOfFile(FILE *file1, FILE *file2, Students *StudFromSec)
 		fscanf(file1, "%s", string);
 		if (string[0] != '\0')
 		{
-			if (ParametrOfError(ValidityOfString(string, 6), 6) == -1)   //РџСЂРѕРІРµСЂРєР° СЃС‚СЂРѕРєРё РёР· 1РѕРіРѕ С„Р°Р№Р»Р° РЅР° РІР°Р»РёРґРЅРѕСЃС‚СЊ
+			if (ParametrOfError(ValidityOfString(string, 6), 6) == -1)   //Проверка строки из 1ого файла на валидность
 			{
 				return -2;
 			}
+			if (SaveStudF1(string, StudFromFir, i1) == -1)
+				return -6;
 			i1++;
 			string[0] = '\0';
 		}
@@ -157,28 +193,28 @@ int ValidOfFile(FILE *file1, FILE *file2, Students *StudFromSec)
 		fscanf(file2, "%s", string);
 		if (string[0] != '\0')
 		{
-        	if (ParametrOfError(ValidityOfString(string, 3), 3) == -1)   //РџСЂРѕРІРµСЂРєР° СЃС‚СЂРѕРєРё РёР· 2РѕРіРѕ С„Р°Р№Р»Р° РЅР° РІР°Р»РёРґРЅРѕСЃС‚СЊ
+			if (ParametrOfError(ValidityOfString(string, 3), 3) == -1)   //Проверка строки из 2ого файла на валидность
 			{
 				return -2;
 			}
-			if (SaveStud(string, StudFromSec, i2)==-1)
-		                return -4;
+			if (SaveStudF2(string, StudFromSec, i2) == -1)
+				return -7;
+			//printf("%s %s %d\n", StudFromSec[i2].Surname, StudFromSec[i2].Name, StudFromSec[i2].Score);
 			i2++;
 			string[0] = '\0';
 		}
 	}
-	fclose(file1);
-	fclose(file2);
 	free(string);
 	if (i1 == i2)
 		return i1;
 	return -1;
 }
 
-void BeforeEnd(FILE *file1, FILE *file2, Students *StudFromSec)
+//Закрывает файлы и освобождет память
+//Принимает указатели на файлы и на массив студентов
+void BeforeEnd(StudentsF1 *StudFromFir, StudentsF2 *StudFromSec)
 {
-	fclose(file1);
-	fclose(file2);
+	free(StudFromFir);
 	free(StudFromSec);
 }
 
@@ -190,62 +226,87 @@ int main()
 	input1 = fopen("input_file_1.csv", "r");
 	input2 = fopen("input_file_2.csv", "r");
 
-	Students *StudFromSec = (Students *)calloc(MAX_NUM, sizeof(Students));
+	StudentsF1 *StudFromFir = (StudentsF1 *)calloc(MAX_NUM, sizeof(StudentsF1));
+	StudentsF2 *StudFromSec = (StudentsF2 *)calloc(MAX_NUM, sizeof(StudentsF2));
 
-	int Error = ValidOfFile(input1, input2, StudFromSec);
-
-	switch(Error)
+	int Error = ValidOfFile(input1, input2, StudFromFir, StudFromSec);
+		
+	switch (Error)
 	{
-	case -1:
+	case -1:												//Разное кол-во студентов в файлах
 	{
-        	printf("Failed with <Different Numbers of Students>\n");
+		printf("Failed with <Different Numbers of Students>\n");
+		BeforeEnd(StudFromFir, StudFromSec);
+		system("pause");
+		return 0;
+	}
+	case -2:												//строка не корректна
+	{
+		BeforeEnd(StudFromFir, StudFromSec);
+		system("pause");
+		return 0;
+	}
+	case -3:												//файлы не существуют
+	{
+		printf("input_file_1.csv doesn't exist\ninput_file_2.csv doesn't exist\n");
+		BeforeEnd(StudFromFir, StudFromSec);
+		system("pause");
+		return 0;
+	}
+	case -4:												//1ый файл не существует
+	{
+		printf("input_file_1.csv doesn't exist\n");
+		fclose(input2);
+		BeforeEnd(StudFromFir, StudFromSec);
+		system("pause");
+		return 0;
+	}
+	case -5:												//2ой файл не существует
+	{
+		printf("input_file_2.csv doesn't exist\n");
+		fclose(input1);
+		BeforeEnd(StudFromFir, StudFromSec);
+		system("pause");
+		return 0;
+	}
+	case -6:
+	{
+		printf("Failed with <One name for a few students. File №1>\n");
 		BeforeEnd(input1, input2, StudFromSec);
-	        return 0;
-        }
-	case -4:
-        {
-        	printf("Failed with <One name for a few students. File 2>\n");
+		system("pause");
+		return 0;
+	}
+	case -7:
+	{
+		printf("Failed with <One name for a few students. File №2>\n");
 		BeforeEnd(input1, input2, StudFromSec);
-	        return 0;
-        }
-	case -2:
-	case -3:
-        {
-            return 0;
-        }
-	default: break;
+		system("pause");
+		return 0;
+	}
+	default: 
+	{
+		fclose(input1);
+		fclose(input2);
+		break;
+	}
 	}
 
 	int NumOfStud = Error;
-	input2 = fopen("input_file_2.csv", "r");
 
 	int MaxScore = 0, count = 0;
-	int i;
-	for(i=0; i<NumOfStud; i++)
+	for (int i = 0; i<NumOfStud; i++)
 	{
-		input1 = fopen("input_file_1.csv", "r");
-		int ScoreOfStud = Search(input1, StudFromSec[i]);                    //РџРѕРёСЃРє СЃС‚СѓРґРµРЅС‚Р° РёР· 2РѕРіРѕ С„Р°Р№Р»Р° РІ 1РѕРј
-		switch (ScoreOfStud)                                                //Р•СЃР»Рё РІРѕР·РІСЂР°С‰РµРЅРЅРѕРµ Р·РЅР°С‡РµРЅРёРµ РЅРµ Р±Р°Р»Р» СЃС‚СѓРґРµРЅС‚Р° - РєРѕРЅРµС†
+		int ScoreOfStud = Search(StudFromFir, StudFromSec[i], NumOfStud);            //Поиск студента из 2ого файла в 1ом
+		
+		if (ScoreOfStud == -1)														//Если возвращенное значение не балл студента - конец
 		{
-			case -1:
-			{
-				printf("Failed with <Student isn't found>\n");
-				BeforeEnd(input1, input2, StudFromSec);
-				return 0;
-			}
-			case -2:
-			{
-				printf("Failed with <One name for a few students. File 1>\n");
-				BeforeEnd(input1, input2, StudFromSec);
-				return 0;
-			}
-			case -3:
-			{
-				BeforeEnd(input1, input2, StudFromSec);
-				return 0;
-			}
+			printf("Failed with <Student isn't found>\n");
+			BeforeEnd(StudFromFir, StudFromSec);
+			system("pause");
+			return 0;
 		}
-		if (ScoreOfStud > MaxScore)
+
+		if (ScoreOfStud > MaxScore)													//Сравнение балла студента с максимальным
 		{
 			MaxScore = ScoreOfStud;
 			count = 1;
@@ -255,8 +316,8 @@ int main()
 		fclose(input1);
 	}
 
-	printf("%d\n", count);
-	fclose(input2);
-	free(StudFromSec);
+	printf("Max=%d, Count=%d\n", MaxScore, count);
+	BeforeEnd(StudFromFir, StudFromSec);
+	system("pause");
 	return 0;
 }
