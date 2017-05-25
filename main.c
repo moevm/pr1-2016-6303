83977ae18bdf4843090ab76c46ae136c8a442a40
jenesius <lokargenia@gmail.com>
2017-05-25 15:22:41 +0300

    NAVAIERA

--- main.c
+++ main.c
@@ -2,20 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-int flag = -1;
 
-void push(char ** stack, char * element){   // Помещаем в СТЭК
-    flag++;
-    strcpy(stack[flag],element);
+void push(char ** stack, char * element,int* flag){   // Помещаем в СТЭК
+    (*flag)++;
+    strcpy(stack[*flag],element);
 }
-void pop(char ** stack){ // Удаляем последний элемент СТЭКа
-    flag--;
+char *top(char ** stack,int* flag){
+    return stack[*flag];
 }
-int size_s(){ // Возвращает размер СТЭКа
-    return flag;
+void pop(char ** stack,int* flag){ // Удаляем последний элемент СТЭКа
+    (*flag)--;
 }
-int empty_s(){ //     Возвращает 0 ,если стэк пуст
-    if (flag == -1){//Возвращает 1 ,если стэк не пуст
+int size_s(char ** stack,int* flag){ // Возвращает размер СТЭКа
+    return *flag;
+}
+int empty_s(char ** stack,int* flag){ //     Возвращает 0 ,если стэк пуст
+    if ((*flag) == -1){//Возвращает 1 ,если стэк не пуст
         return 0;
     }
     else{
@@ -24,47 +26,50 @@ int empty_s(){ //     Возвращает 0 ,если стэк пуст
 }
 int main()
 {
-    char c,tag[80];
+
+    char c,cup[80];
     int i;
     /*Выделение памяти под стэк*/
-    char **stack;
-    stack = (char**)malloc(100 * sizeof(char*));
+    char **tag;
+    tag = (char**)malloc(100 * sizeof(char*));
         for (i = 0; i<100; i++){
-                stack[i] = (char*)malloc(80 * sizeof(char));
+                tag[i] = (char*)malloc(80 * sizeof(char));
         }
      /**/
+
     while ((c = getchar()) != EOF ){
 
         if (c == '<') {
-                        scanf("%79[^>]>",tag); //Считываем ИМЯ тэга
+                        scanf("%79[^>]>",cup); //Считываем ИМЯ тэга
 
-                        if (strcmp(tag,"hr") && strcmp(tag,"br")){
+                        if (strcmp(cup,"hr")  && strcmp(cup,"br") ){
 
-                                if (tag[0] != '/'){ // Если тэг - открывающийся : заносим в стэк
-                                                    push(stack,tag);
+                                if (cup[0] != '/'){ // Если тэг - открывающийся : заносим в стэк
+                                                    push(tag,cup,&flag);
                                                     }
             else{
-                    if (empty_s() == 0){    // Если стэк пуст, то есть ЗАКРВАЮЩИЙСЯ  тэг не был открыт, то выход
+                    if (empty_s(tag,&flag) == 0){    // Если стэк пуст, то есть ЗАКРВАЮЩИЙСЯ  тэг не был открыт, то выход
                                 printf("wrong");
                                 return 0;
                                     }
                                 else{
-                                        if (strcmp(stack[flag],tag+1) != 0){  // Если закрывающийся, не равен перед ним стоящему открывающемуся
+                                        if (strcmp(top(tag,&flag),cup+1) != 0){  // Если закрывающийся, не равен перед ним стоящему открывающемуся
                                                     printf("wrong");
                                                     return 0;
                                                                         }
                                                                         else{   // Если тэги совпадают
-                                                                            pop(stack);
+                                                                            pop(tag,&flag);
                                                                             }
                                         }
                 }
                                                                     }
                     }
                                         }
-    if (empty_s() == 0){ // Если стэк пуст, то страницы ВАЛИДНА
+    if (empty_s(tag,&flag) == 0){ // Если стэк пуст, то страницы ВАЛИДНА
         printf("correct");
     }
     else{
        printf("wrong");
     }
+    free(tag);
 }
